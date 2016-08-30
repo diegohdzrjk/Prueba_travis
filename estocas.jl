cf30ec15a454281df01337bb0e66f8ea04cb50ba
Diego Hernandez <dhernandezrjk@ciencias.unam.mx>
2016-08-30 10:58:08 -0500

    se agrega una funcion para checar si una matriz es estocastica

--- estocas.jl
+++ estocas.jl
@@ -1,4 +1,3 @@
-
 function estocka(n, m)
     M = zeros(Float64, n, m) 
     for i in 1:n
@@ -9,4 +8,17 @@ function estocka(n, m)
     return M
 end
 
+
+function estocka_check(M)
+    n = size(M)[1]
+    m = size(M)[2]
+    
+    for i in 1:n
+        suma = sum(M[i,:])
+        if abs(suma-1.0) > 1.0e-5
+            return false
+        end
+    end
+    return true
+end
 
